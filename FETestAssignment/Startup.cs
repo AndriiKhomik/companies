4406e8035dffa36d6e187b128578308656d7dd49
Andrii <satvaer@gmail.com>
2021-12-21 14:39:08 +0200

    initial project

---
+++ FETestAssignment/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using FETestAssignment.Data;
+
+namespace FETestAssignment
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+            
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "FETestAssignment", Version = "v1"});
+                
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddDbContext<FETestAssignmentContext>(options =>
+                    options.UseInMemoryDatabase("FETestAssignmentContext"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FETestAssignment v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors();
+            
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# FE test assignment
+
+## Assignment
+
+Create a front end application using React that displays some companies from public registry and lets user to add them
+to our system.
+
+Exact UI to implement is linked below. It should take Material as a base.
+https://www.figma.com/file/3AeOmmqn8lLFaouHFrGZLh/Test?node-id=1%3A2307 .
+It's best to create account there and log in to take full advantage of Figma.
+
+### Before starting the assignment
+
+Send me your estimate, how long does it take for you and when it might be finished.
+
+If you have any questions before or during the assignment don't hesitate to ask.
+
+### After completing the assignment
+
+Send me your code or link to repository. Also give some feedback about the assignment, how hard it was for you and what
+you would do differently.
+
+## Setting up the back end
+
+Install [.Net 5.0 SDK](https://dotnet.microsoft.com/download).
+
+Clone the repository to your computer and run the following in repository directory:
+
+    cd FETestAssignment
+    dotnet build
+    dotnet run
+
+Open the API description in browser https://localhost:5001/swagger/index.html .
+
+To reset the data, stop the application and start again.
