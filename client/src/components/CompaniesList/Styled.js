2541ada33dc8ce53dfbd51199cea900f54ccef69
Andrii <satvaer@gmail.com>
2021-12-21 21:42:15 +0200

    place content to center

--- client/src/components/CompaniesList/Styled.js
+++ client/src/components/CompaniesList/Styled.js
@@ -4,6 +4,7 @@ import { styled } from "@mui/system";
 export const StyledList = styled(List)`
   display: grid;
   grid-template-columns: repeat(auto-fill, 370px);
+  justify-content: center;
   row-gap: 16px;
   column-gap: 16px;
   margin-top: 72px;
